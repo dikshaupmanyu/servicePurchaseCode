fd1a7c3e45550d9cd92c322af3d502a24dbec394
Diksha Upmanyu <diksha.u@vsple.com>
2022-06-23 00:41:00 +0530

    first commit

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ var cors = require('cors');
 var request = require('request');
 var http = require('http');
 var https = require('https');
-var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
-var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
-var credentials = {key: privateKey, cert: certificate};
 var expressJwt = require('express-jwt');
 var jwt = require('jsonwebtoken');
 var bodyParser = require('body-parser');
@@ -22,24 +19,15 @@ var bodyParser = require('body-parser');
 
 	app.set('view engine', 'ejs'); // set up ejs for templating
 	app.use(express.static(path.join(__dirname, 'public')));
-	app.use(express.static('dist'));
 
+	 app.get('/servicesdetail', function(req, res) {
 
-// routes ======================================================================
-//require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+	 	var serviceIddd = req.query.id;
 
-// launch ======================================================================
-// app.listen(port);
-
-
- app.get('/servicesdetail', function(req, res) {
-
- 	var serviceIddd = req.query.id;
-
-    
-    res.render('servicesdetail.ejs' , {serviceIdDetail : serviceIddd });
-	     
- });
+	    
+	    res.render('servicesdetail.ejs' , {serviceIdDetail : serviceIddd });
+		     
+	 });
 
   
   
